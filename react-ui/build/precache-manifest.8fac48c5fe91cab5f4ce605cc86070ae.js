eafdf78b0ba08f1d8984519ee905d80d9b28ef3d
Sigrid Aasma <sigridaasma@gmail.com>
2018-11-29 00:34:37 +0200

    update dependencies

---
+++ react-ui/build/precache-manifest.8fac48c5fe91cab5f4ce605cc86070ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b1a8f33377e68089ac373df35b408a6",
+    "url": "/static/media/spiritisland.1b1a8f33.png"
+  },
+  {
+    "revision": "b35473611a2eef3b11fb",
+    "url": "/static/css/main.557d947d.chunk.css"
+  },
+  {
+    "revision": "c5495f04704c7c5ead482ee26bd7183c",
+    "url": "/static/media/LogoandSlogan.c5495f04.png"
+  },
+  {
+    "revision": "5c38213ac4c4c5a30010",
+    "url": "/static/js/1.5c38213a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b35473611a2eef3b11fb",
+    "url": "/static/js/main.b3547361.chunk.js"
+  },
+  {
+    "revision": "c282eb57f14d5afa4b34ab7636f9833e",
+    "url": "/static/media/pinkcircle2.c282eb57.svg"
+  },
+  {
+    "revision": "4f6ae20eab13928c99e55634d6a06b3f",
+    "url": "/static/media/LIKEMINDED_HEADING_LOGOV3.4f6ae20e.png"
+  },
+  {
+    "revision": "dc7c90bbae919e07baa89898e99e2c65",
+    "url": "/static/media/home-page-people.dc7c90bb.png"
+  },
+  {
+    "revision": "5c38213ac4c4c5a30010",
+    "url": "/static/css/1.7a6e4b26.chunk.css"
+  },
+  {
+    "revision": "84b35de91cf2555edd2f5e49256ffc93",
+    "url": "/index.html"
+  }
+];
